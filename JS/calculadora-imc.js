1869bee81cf6f7c0082af01c435ec898b23c8b22
kahuanPR <kahuan.siq@icloud.com>
2024-02-28 11:47:24 -0300

    Configurando calculadora IMC

--- JS/calculadora-imc.js
+++ JS/calculadora-imc.js
@@ -1,4 +1,4 @@
-//capturar os evebtos do formulario
+//capturar os eventos do formulario
 const formulario = document.querySelector('#formulario');
 
 formulario.addEventListener('submit', function(e){
@@ -18,7 +18,11 @@ formulario.addEventListener('submit', function(e){
 
     const imc = calcularimc(peso, altura)
     console.log(imc)
+    const classificacao = tabelaImc(imc)
+
+    mostrarResultado(classificacao)
 })
+
 
 //função para calcular o imc e retornar o valor do imc com duas casas decimais
 function calcularimc(peso, altura){
@@ -26,4 +30,19 @@ function calcularimc(peso, altura){
     return imc.toFixed(2);
 
 }
+function mostrarResultado(msg){
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = msg
+}
+
+function tabelaImc(imc){
+    const classificacao = ['Abaixo do peso', 'Peso normal', 'Sobrepreso','Obesidade 1', 'Obesidade 2', 'Obesidade 3']
+    if (imc >= 39.9) return classificacao[5];
+    if (imc >= 34.9) return classificacao[4];
+    if (imc >= 29.9) return classificacao[3];
+    if (imc >= 24.9) return classificacao[2];
+    if (imc >= 18.5) return classificacao[1];
+    if (imc < 18.5) return classificacao[0];
+}
+
  
