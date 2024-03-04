713ee1e2c5542097de238da69445b6d9e4522ce3
kahuanPR <kahuan.siq@icloud.com>
2024-03-04 11:56:16 -0300

    AULA 09

---
+++ Aula 09/scripts/imagem.js
@@ -0,0 +1,32 @@
+let images = []; /*criando uma lista vazia para guardar as imagens */
+
+function carregarImagem(){
+    const galeria = document.getElementById("galeria");
+    galeria.innerHTML = '';
+    console
+
+    images.forEach(imgURL=>{
+        const img = document.createElement("img");
+        img.src = imgURL;
+        galeria.appendChild(img)
+    });
+
+}
+
+document.getElementById('arquivos').addEventListener('change', function(event){
+    const arquivo = event.target.files[0];
+    const ler = new FileReader();
+
+    ler.onload = function(e){
+        const imgURL = e.target.result;
+        images.push(imgURL);
+        carregarImagem();
+
+    };
+
+    ler.readAsDataURL(arquivo)
+
+
+
+
+});
